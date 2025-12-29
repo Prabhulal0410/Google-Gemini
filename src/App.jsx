94452f3282e3d3bc5612c3d16232a2c91c7d711a
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-29 21:57:01 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p className='bg-red-600'>hello</p>
+      <p className='bg-gray-200 font-bold text-center'>Google-Gemini</p>
     </>
   )
 }
