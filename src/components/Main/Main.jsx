7d2f3b7b0d58c33ae66dff96e54a15ebe094bbc1
Prabhulal <prabhulal.raghwani410@gmail.com>
2025-12-31 22:46:37 +0530

    added navbar for main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import "./Main.css"
 
 const Main = () => {
   return (
     <div className='main'>
-        Main
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.user_icon} alt="" />
+        </div>
     </div>
   )
 }
