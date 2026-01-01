0effdffc666a6387680e79fb8da690abc824202d
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-01-01 22:05:11 +0530

    added transition in sidebar when open and close

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -5,40 +5,47 @@ import { useState } from "react";
 const Sidebar = () => {
   const [extended, setExtended] = useState(false);
 
-    const handleSidebarToggle = () => {
-        setExtended(!extended)
-    }
-
   return (
-    <div className="sidebar">
+    <div className={`sidebar ${extended ? "extended" : ""}`}>
+      {/* TOP */}
       <div className="top">
-        <img className="menu" src={assets.menu_icon} alt="" onClick={handleSidebarToggle}/>
+        <img
+          className="menu"
+          src={assets.menu_icon}
+          alt="menu"
+          onClick={() => setExtended(!extended)}
+        />
+
         <div className="new-chat">
-          <img src={assets.plus_icon} alt="" />
-          {extended ? <p>New Chat</p> : null}
+          <img src={assets.plus_icon} alt="new chat" />
+          <p className="text">New Chat</p>
         </div>
-        {extended ? (
-          <div className="recent">
-            <p className="recent-title">Recent</p>
-            <p className="recent-entry">
-              <img src={assets.message_icon} alt="" />
-              <p>What is React ...</p>
-            </p>
+
+        <div className="recent">
+          <p className="recent-title text">Recent</p>
+
+          <div className="recent-entry">
+            <img src={assets.message_icon} alt="message" />
+            <p className="text">What is React ...</p>
           </div>
-        ) : null}
+        </div>
       </div>
+
+      {/* BOTTOM */}
       <div className="bottom">
         <div className="bottom-item recent-entry">
-          <img src={assets.question_icon} alt="" />
-          {extended ? <p>Help</p> : null}
+          <img src={assets.question_icon} alt="help" />
+          <p className="text">Help</p>
         </div>
+
         <div className="bottom-item recent-entry">
-          <img src={assets.history_icon} alt="" />
-          {extended ? <p>Activity</p> : null}
+          <img src={assets.history_icon} alt="activity" />
+          <p className="text">Activity</p>
         </div>
+
         <div className="bottom-item recent-entry">
-          <img src={assets.setting_icon} alt="" />
-          {extended ? <p>Settings</p> : null}
+          <img src={assets.setting_icon} alt="settings" />
+          <p className="text">Settings</p>
         </div>
       </div>
     </div>
