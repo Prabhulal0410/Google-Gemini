6ef6edd6b6e7701f56f964a3ddd6cec67027dccb
Prabhulal <prabhulal.raghwani410@gmail.com>
2026-01-03 22:16:19 +0530

    install googleai sdk and added code for ai

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,1 +1,34 @@
-const API_KEY = "AIzaSyDIlkOxXJtYyqbZRvQd9Mi67ni10dW4aMU"
+// geminiChat.js
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
+
+const model = "gemini-3-pro-preview";
+
+// Create chat session
+export function createChat() {
+  const contents = [];
+
+  return async function sendMessage(prompt) {
+    contents.push({
+      role: "user",
+      parts: [{ text: prompt }],
+    });
+
+    const response = await ai.models.generateContent({
+      model,
+      contents,
+    });
+
+    const answer = response.text;
+
+    contents.push({
+      role: "model",
+      parts: [{ text: answer }],
+    });
+
+    return answer;
+  };
+}
